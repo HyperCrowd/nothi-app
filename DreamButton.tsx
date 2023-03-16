449dbc4e3ba038c3932c58199fbdff958a1bf224
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-03-15 21:16:19 -0400

    2 files modified

--- DreamButton.tsx
+++ DreamButton.tsx
@@ -9,7 +9,7 @@ export default function DreamButton({ avatar }: Props) {
   const [isRecording, setIsRecording] = useState(false);
 
   const style = {
-    backgroundImage: `url(https://raw.githubusercontent.com/HyperCrowd/nothi-app/main/assets/dream_button_raised.png)`,
+    backgroundImage: `url(https://raw.githubusercontent.com/HyperCrowd/nothi-app/main/assets/dream-button.png)`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
@@ -20,7 +20,7 @@ export default function DreamButton({ avatar }: Props) {
   return (
     <div className="guruAvatar main-bkg">
       <div
-        className="dreamButton"
+        className="dreamButton shadow"
         style={style}
         onClick={() => {
           setIsRecording(!isRecording);
