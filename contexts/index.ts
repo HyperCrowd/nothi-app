7e20ae0d0af481b77e6e5b0f309c4ba39dabcf80
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-05-26 12:55:28 -0400

    1 file modified

--- contexts/index.ts
+++ contexts/index.ts
@@ -1,3 +1,47 @@
 export interface Action {
   type: string;
 }
+
+export interface Context<State extends Object> {
+  state: State;
+  dispatch: {
+    type: string;
+  } & State;
+}
+
+export default class ContextBus {
+  contexts: {
+    [key: string]: StateType<Object>;
+  } = {};
+
+  constructor() {}
+
+  /**
+   *
+   */
+  addContext<StateType extends Object>(
+    name: string,
+    context: Context<StateType>
+  ) {
+    this.contexts[name] = context;
+  }
+
+  /**
+   *
+   */
+  schedule(
+    name: string,
+    action: (
+      context: Context<typeof this.contexts[name]>,
+      ...args: any[]
+    ) => void
+  ) {
+    const context = this.contexts[name];
+
+    if (context === undefined) {
+      throw new RangeError(`${name} is not a valid context`);
+    }
+
+    action(context, ...args);
+  }
+}
